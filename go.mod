5ca1656aaa5d624625045e3a86796b8cbc00e095
AtrasYahza <alfatrasyahza@gmail.com>
2023-03-31 11:52:17 +0800

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/AtrasYahza/go-say-hello
+module github.com/AtrasYahza/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello setan kredit"
+func SayHello(name string) string {
+	return "Hello setan " + name
 }
